70a0dd8eca7fa08a3c5c8fb9b96caf452eced9b1
kirankm <kiran.k.madan@gmail.com>
2020-08-17 16:56:00 +0530

    finishing problems in arrays

---
+++ additioanl_exercise/arrays/ex_1.rb
@@ -0,0 +1,3 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+puts "I have a pet #{my_pet = pets[2]}"
+puts my_pet
---
+++ additioanl_exercise/arrays/ex_3.rb
@@ -0,0 +1,4 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+my_pets.delete("lizard")
+puts my_pets
