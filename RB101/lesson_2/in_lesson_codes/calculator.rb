bc33f87b0533b9627a94a94e15cb411ad8047453
kirankm <kiran.k.madan@gmail.com>
2020-08-21 19:01:29 +0530

    calculator.rb

--- RB101/lesson_2/in_lesson_codes/calculator.rb
+++ RB101/lesson_2/in_lesson_codes/calculator.rb
@@ -74,13 +74,13 @@ loop do
 
   result = case operator
            when "1"
-             number1.to_i + number2.to_i
+             number1.to_f + number2.to_f
            when "2"
-             number1.to_i - number2.to_i
+             number1.to_f - number2.to_f
            when "3"
-             number1.to_i * number2.to_i
+             number1.to_f * number2.to_f
            when "4"
-             number1.to_f / number2.to_i
+             number1.to_f / number2.to_f
            end
 
   prompt("The result is #{result}")
