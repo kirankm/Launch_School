d59b5848241a047705e3d746a17aeb93e82b01e2
kirankm <kiran.k.madan@gmail.com>
2020-08-21 15:00:41 +0530

    adding option for continuous evaluation

--- RB101/lesson_2/in_lesson_codes/calculator.rb
+++ RB101/lesson_2/in_lesson_codes/calculator.rb
@@ -26,34 +26,40 @@ loop do
 end
 prompt("Hi #{name}")
 
-number1 = 0
 loop do
-  prompt("What's the first number?")
-  number1 = Kernel.gets().chomp()
-  break if is_valid_number(number1)
-  puts "Hmm that doesn't look like a valid number"
+  number1 = 0
+  loop do
+    prompt("What's the first number?")
+    number1 = Kernel.gets().chomp()
+    break if is_valid_number(number1)
+    puts "Hmm that doesn't look like a valid number"
+  end
+
+  number2 = 0
+  loop do
+    prompt("What's the second number?")
+    number2 = Kernel.gets().chomp()
+    break if is_valid_number(number2)
+    puts "Hmm that doesn't look like a valid number"
+  end
+
+  prompt("What's the opeation to perform? 1) add 2)subtract 3)multiply 4 divide")
+  operator = Kernel.gets().chomp()
+
+  result = case operator 
+            when "1"
+              number1.to_i + number2.to_i
+            when "2"
+              number1.to_i - number2.to_i
+            when "3"
+              number1.to_i * number2.to_i
+            when "4"
+              number1.to_f/number2.to_i
+  end
+
+  prompt("The result is #{result}")
+
+  prompt("Hi #{name}, would you like to continue using the calculator. Type Y for continuing")
+  response = Kernel.gets().chomp()
+  break unless response.downcase().start_with?("y")
 end
-
-number2 = 0
-loop do
-  prompt("What's the second number?")
-  number2 = Kernel.gets().chomp()
-  break if is_valid_number(number2)
-  puts "Hmm that doesn't look like a valid number"
-end
-
-prompt("What's the opeation to perform? 1) add 2)subtract 3)multiply 4 divide")
-operator = Kernel.gets().chomp()
-
-result = case operator 
-          when "1"
-            number1.to_i + number2.to_i
-          when "2"
-            number1.to_i - number2.to_i
-          when "3"
-            number1.to_i * number2.to_i
-          when "4"
-            number1.to_f/number2.to_i
-end
-
-prompt("The result is #{result}")
