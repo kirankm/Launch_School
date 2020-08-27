985ddbca5182a077d4099a6be742fd3c64008857
kirankm <kiran.k.madan@gmail.com>
2020-08-27 17:22:57 +0530

    refactoring loan_calculator

--- RB101/lesson_2/loan_calculator/loan_calculator.rb
+++ RB101/lesson_2/loan_calculator/loan_calculator.rb
@@ -9,99 +9,126 @@ Output
   M = P*(J/(1-(I+J)**(-N)))
 
 =end
+require "yaml"
+
 LANGUAGE = "en"
 
-require "yaml"
-require "pry"
 
 CONFIG = YAML.load(File.read("loan_config.yml"))
 
 ### Helper Functions
+def prompt(*msg)
+  puts "==> #{msg.join(" ")}"
+end
 
-def prompt(msg, lang = "en")
-  Kernel.puts("==> #{CONFIG[lang][msg]}")
+def get_message(msg, lang = "en")
+  CONFIG[lang][msg]
 end
 
 def valid_name?(name)
   (name.to_i == 0) && (name.length > 0) && (name.upcase != name.downcase)
 end
 
-def valid_loan_amt?(amount)
-  amount > 0
+def is_positive_float?(num)
+  begin
+    (num.to_f >= 0) and Float(num)
+  rescue ArgumentError
+    false
+  end
 end
 
-def valid_loan_duration?(duration)
-  duration > 0
+def is_positive_int?(num)
+  begin
+    (num.to_i >= 0) and Integer(num)
+  rescue ArgumentError
+    false
+  end
 end
 
-def valid_loan_rate?(rate)
-  rate > 0 && rate <= 100
-end
 
-def monthly_rate(annual_rate)
-  annual_rate/12.0
-end
-
-def total_months(no_years)
-  no_years * 12
-end
-
-def monthly_pay(amt, years, annual_rate)  
-  duration = total_months(years)
-  int_rate = monthly_rate(annual_rate)/100
-
-  mon_installment = amt*(int_rate/term3)
-  return mon_installment
+def monthly_pay(amount, months, monthly_rate)
+  monthly_installment = amount.to_f* (monthly_rate / (1 - (1 + monthly_rate)**(-months)))
+  return 0 if monthly_installment.nan?
+  monthly_installment
 end
 
 ### Main Program
-prompt(:welcome)
+prompt(get_message(:welcome))
 
 ## Get Name
 name = gets.chomp()
 
 while !valid_name?(name)
-  prompt(:valid_name)
+  prompt(get_message(:valid_name))
   name = gets.chomp()
 end
 
-puts "==> Hi #{name}"
+prompt("Hi", "#{name.capitalize}") 
 
 loop do
   ## Get Loan Amount
-  prompt(:loan_amount)
-  loan_amount = gets.chomp().to_i()
-
-  while !valid_loan_amt?(loan_amount)
-    prompt(:valid_loan_amt)
-    loan_amount = gets.chomp().to_i()
-  end
-
-  # Get Loan Duration
-  prompt(:loan_duration)
-  loan_duration = gets.chomp().to_f()
-
-  while !valid_loan_duration?(loan_duration)
-    prompt(:valid_loan_dur)
-    loan_duration = gets.chomp().to_f()
+  prompt(get_message(:loan_amount))
+  
+  loan_amount = gets.chomp()
+  while !is_positive_float?(loan_amount)
+    prompt(get_message(:valid_loan_amt))
+    loan_amount = gets.chomp()
   end
 
   # Get Interest Rate
-  prompt(:loan_rate)
-  loan_rate = gets.chomp().to_f()
-
-  while !valid_loan_rate?(loan_rate)
-    prompt(:valid_loan_rate)
-    loan_rate = gets.chomp().to_f()
+  prompt(get_message(:loan_rate))
+  
+  loan_rate = gets.chomp()
+  while !is_positive_float?(loan_rate)
+    prompt(get_message(:valid_loan_rate))
+    loan_rate = gets.chomp()
   end
 
+  # Get Loan Duration
+  loan_years = ""
+  loan_months = ""
+  loop do
+    # Get Loan Years
+    prompt(get_message(:loan_years))
+    
+    loan_years = gets.chomp()
+    while !is_positive_int?(loan_years)
+      prompt(get_message(:valid_loan_years))
+      loan_duration = gets.chomp().to_f()
+    end
 
-  puts "==> Loan amt is #{loan_amount} and duration is #{loan_duration} and rate is #{loan_rate}"
+    # Get Loan Months
+    prompt(get_message(:loan_months))
+    
+    loan_months = gets.chomp()
+    while !is_positive_int?(loan_months)
+      prompt(get_message(:valid_loan_months))
+      loan_duration = gets.chomp().to_f()
+    end
 
-  monthly_installment = monthly_pay(loan_amount, loan_duration, loan_rate)
-  puts "==> The monthly installment to be paid #{monthly_installment}"
+    break if (loan_years.to_i > 0) || (loan_months.to_i > 0)
+    prompt("The total Loan Term cannot be 0 Years and 0 Months")
+  end
 
-  prompt(:continue)
+  prompt("#"*120)
+  prompt("Your Loan details are given below")
+  prompt("Loan amt is:", "#{loan_amount}")
+  prompt("Loan rate is:", "#{loan_rate}")
+  prompt("Loan Term is:", "#{loan_years} Years and", "#{loan_months} Months")
+  
+
+  # Calculating Monthly Installment
+  annual_interest_rate = loan_rate.to_f/100
+  monthly_intest_rate = annual_interest_rate/12
+  total_months = 12*loan_years.to_i + loan_months.to_i
+
+  monthly_installment = monthly_pay(loan_amount, total_months, monthly_intest_rate)
+  prompt("The monthly installment to be paid is" ,"#{monthly_installment}")
+
+  prompt(get_message(:continue))
   response = gets.chomp[0].downcase
   break unless response == "y"
 end
+
+prompt "Thank you for using the Mortgage Calculator!"
+prompt "Good bye!"
